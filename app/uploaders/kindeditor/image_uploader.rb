f5215108d0f5a79d4a627e60a440aaef860e7d2b
suixuexiang <13910396471@qq.com>
2014-10-15 18:44:42 +0800

    Add the image raito mechanism for image preview

--- app/uploaders/kindeditor/image_uploader.rb
+++ app/uploaders/kindeditor/image_uploader.rb
@@ -4,5 +4,19 @@ class Kindeditor::ImageUploader < Kindeditor::AssetUploader
     EXT_NAMES[:image]
   end
 
+  def filename
+    if original_filename+
+      @name ||= "#{image_ratio(file)}" + Digest::MD5.hexdigest(File.dirname(current_path)).slice(0, 12)
+      "#{@name}.#{file.extension}"
+    end
+  end
+
+  def image_ratio(file)
+    if file
+      image = MiniMagick::Image.open(file.path)
+      "ratio#{image['width']/image['height']}_"
+    end
+  end
+
 end
 
